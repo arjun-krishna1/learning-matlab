f39b52e59a4e2f1a691d9c48193091e36abf7f02
arjun_krishna <45014214+arjun-krishna1@users.noreply.github.com>
2021-09-16 17:45:43 -0400

    Create vars_arithmetic.m

---
+++ vars_arithmetic.m
@@ -0,0 +1,6 @@
+clear all; % clears workspace
+clc; % clears the command window
+a=20; % initialize a to 20
+b=input('please enter the value of variable'); % get input from user
+c=a*b; % simple arithmetic, multiplication
+display(c); % show this as result
